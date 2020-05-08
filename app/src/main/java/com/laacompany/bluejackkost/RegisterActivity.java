3520ae457d065382d5b076a0c16cfe0675de571e
Dragnalio <handikalimanto@gmail.com>
2020-05-09 05:14:27 +0700

    Added Register Validation Modified Register Layout Added correct input types Minor grammar fixes

--- app/src/main/java/com/laacompany/bluejackkost/RegisterActivity.java
+++ app/src/main/java/com/laacompany/bluejackkost/RegisterActivity.java
@@ -60,7 +60,7 @@ public class RegisterActivity extends AppCompatActivity {
         String cpassword = mETCPassword.getText().toString();
         String phone = mETPhone.getText().toString();
         String dob = "20/03/2000";
-        boolean isMale = mRBMale.isChecked(), isTermCross = mCBTerms.isChecked();
+        boolean isMale = mRBMale.isChecked(), isTermCross = mCBTerms.isChecked(), valid = true;
 
 
         /*
@@ -69,6 +69,77 @@ public class RegisterActivity extends AppCompatActivity {
 
 
          */
+        boolean usernameHasAlphabet=false, usernameHasNumber=false;
+        for(int i=0;i<username.length();++i){
+            Character character = username.charAt(i);
+            if(usernameHasAlphabet && usernameHasNumber){
+                break;
+            }else if((character>='A' && character<='Z') || (character>='a' && character<='z')){
+                usernameHasAlphabet = true;
+            }else if(Character.isDigit(character)){
+                usernameHasNumber = true;
+            }
+        }
+        if (username.length()<3 || username.length()>25 || !usernameHasAlphabet || !usernameHasNumber) {
+            mETUsername.setError("Username must be 3 to 25 digits containing at least one letter and one digit");
+            valid = false;
+        } else {
+            mETUsername.setError(null);
+        }
+
+        boolean passwordHasUppercase=false, passwordHasLowercase=false , passwordHasNumber=false;
+        for(int i=0;i<password.length();++i){
+            Character character = password.charAt(i);
+            if(passwordHasUppercase && passwordHasLowercase && passwordHasNumber){
+                break;
+            }else if(character>='A' && character<='Z'){
+                passwordHasUppercase = true;
+            }else if(character>='a' && character<='z'){
+                passwordHasLowercase = true;
+            }else if(Character.isDigit(character)){
+                passwordHasNumber = true;
+            }
+        }
+        if (password.length()<7 || !passwordHasUppercase || !passwordHasLowercase || !passwordHasNumber) {
+            mETPassword.setError("Password must be more than 6 digits containing at least one uppercase letter, one lowercase letter, and one digit");
+            valid = false;
+        } else {
+            mETPassword.setError(null);
+        }
+
+        if (!cpassword.equals(password)) {
+            mETCPassword.setError("Password and Confirmation Password must be the same");
+            valid = false;
+        } else {
+            mETCPassword.setError(null);
+        }
+
+        boolean phoneIsNumeric=true;
+        for(int i=0;i<phone.length();++i){
+            Character character = phone.charAt(i);
+            if(!Character.isDigit(character)){
+                phoneIsNumeric=false;
+                break;
+            }
+        }
+        if (phone.length()<10 || phone.length()>12 || !phoneIsNumeric) {
+            mETPhone.setError("Phone number must be 10 to 12 digits containing only numbers");
+            valid = false;
+        } else {
+            mETPhone.setError(null);
+        }
+
+        // VALIDASI GENDER??
+
+        if(!isTermCross){
+            mCBTerms.setError("You must agree with our terms of service to continue");
+            valid = false;
+        } else {
+            mCBTerms.setError(null);
+        }
+
+        if(!valid) return;
+
 
         String user_id = Handler.getUserID();
         Handler.sUsers.add(new User(user_id, username, password, phone, dob, (isMale) ? "Male" : "Female"));
